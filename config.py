2f0c00e39135689378b3cb882836521742d91d15
wclouh-02 <chanwl-am21@student.tarc.edu.my>
2024-09-19 14:54:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.cxq1oxzgpazv.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "chanweilouh-employee1"
+custombucket = "chanweilouh-employee"
 customregion = "us-east-1"
 
