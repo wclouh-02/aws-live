299085f9c6c7b9b4a525d0104f4857964f15b86f
wclouh-02 <chanwl-am21@student.tarc.edu.my>
2024-09-19 14:35:10 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cxq1oxzgpazv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanweilouh-employee1"
+customregion = "us-east-1"
+
